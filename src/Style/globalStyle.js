78fa754bb400f4a7bf5bac1e4e92469221792569
vanessacreis <vanessacreis@live.com>
2022-05-21 13:36:35 -0300

    feat: create global style

---
+++ src/Style/globalStyle.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+  	font-size: 62.5%;
+	}
+
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  html,body{
+    width: 100%;
+    min-height: 100vh;
+  }
+`;
+
+export const colors = {
+  name: "colors",
+  white: "#FCFCFC",
+  black: "#0d0d0d",
+  blue: "#007CC3",
+  gray: "#F2F2F2",
+  darkgray: "#929292",
+};
