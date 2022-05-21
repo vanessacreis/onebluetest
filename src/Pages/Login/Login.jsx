8b612fb86aad349ae0aaea4cce2e8557ec0bffda
vanessacreis <vanessacreis@live.com>
2022-05-21 18:33:00 -0300

    feat login page

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginForm from "../../Components/LoginForm/LoginForm.jsx";
+import * as S from "./login.js";
+
+const Login = () => {
+  return (
+    <S.Main>
+      <LoginForm />
+    </S.Main>
+  );
+};
+
+export default Login;
---
+++ src/Pages/Login/login.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import { colors } from "../../Style/globalStyle.js";
+
+export const Main = styled.main`
+  width: 100vw;
+  height: calc(100vh - 5rem);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-image: linear-gradient(
+      to bottom,
+      transparent 0%,
+      ${colors.gray} 95%
+    ),
+    url(/src/Assets/imgs/bg_login.jpg);
+  background-size: cover;
+`;
