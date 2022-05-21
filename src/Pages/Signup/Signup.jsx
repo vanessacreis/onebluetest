e34937b8ffd3e2d05304d8a3044b7c7bcfdfb296
vanessacreis <vanessacreis@live.com>
2022-05-21 18:32:39 -0300

    feat sign up page created

---
+++ src/Pages/Signup/Signup.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SignupForm from "../../Components/SignupForm/SignupForm.jsx";
+import * as S from "./signup.js";
+
+const Signup = () => {
+  return (
+    <S.Main>
+      <SignupForm />
+    </S.Main>
+  );
+};
+
+export default Signup;
