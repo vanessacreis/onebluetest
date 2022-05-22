1535294ce1fedc357c58c36d759f9c09fa2fd7e6
vanessacreis <vanessacreis@live.com>
2022-05-21 23:21:10 -0300

    fix success page route

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes as Switch, Route } from "react-router-dom";
+import { GlobalStyle } from "../Style/globalStyle.js";
 import Footer from "../Components/Footer/Footer.jsx";
 import Header from "../Components/Header/Header.jsx";
 import Home from "../Pages/Home/Home.jsx";
 import Login from "../Pages/Login/Login.jsx";
 import Signup from "../Pages/Signup/Signup.jsx";
-import { GlobalStyle } from "../Style/globalStyle.js";
+import Success from "../Pages/Sucess/Success.jsx";
 
 const Routes = () => {
   return (
@@ -16,6 +17,7 @@ const Routes = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/success/:name" element={<Success />} />
       </Switch>
       <Footer />
     </BrowserRouter>
