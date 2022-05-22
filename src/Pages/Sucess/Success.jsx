56dc9e8cb3710f403ff0c39739c2c1251053c5e0
vanessacreis <vanessacreis@live.com>
2022-05-22 14:18:58 -0300

    fix included animate

--- src/Pages/Sucess/Success.jsx
+++ src/Pages/Sucess/Success.jsx
@@ -1,29 +1,30 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
-import { Carousel } from "../../Components/Carousel/Carousel.js";
 import * as S from "./Success.js";
+import "animate.css";
 
 const Account = () => {
   const { name } = useParams();
 
   return (
     <S.Main>
-      <div className="col-a">
+      <div className="col-a animate__animated animate__fadeIn">
         <h1>
-          Hi <span className="blueText name">{name}</span>!
+          Hi <span className="blueText">{name}</span>!
         </h1>
         <h2>Welcome to Stories App.</h2>
         <p>
           Using Stories App you can share artworks, memories and much more like
           a pro. Add informations, videos and much more to your creations.
+          <br />
+          Let's show and see only the <span className="blueText">beauty </span>
+          together!
         </p>
         <Link to="/" className="button">
           Soon in your app store.
         </Link>
       </div>
-      <div className="col-b">
-        <Carousel />
-      </div>
+      <div className="col-b"></div>
     </S.Main>
   );
 };
