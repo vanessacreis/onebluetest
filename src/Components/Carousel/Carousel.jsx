339b6916e8c954ce9ed86246be51323d8d0e484d
vanessacreis <vanessacreis@live.com>
2022-05-22 15:03:17 -0300

    feat carousel component created

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -3,17 +3,13 @@ import * as S from "./carousel.js";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import ImgCarousel1 from "../../Assets/imgs/carousel-1.jpg";
-import ImgCarousel2 from "../../Assets/imgs/carousel-2.png";
-import ImgCarousel3 from "../../Assets/imgs/carousel-3.png";
-import ImgCarousel4 from "../../Assets/imgs/carousel-4.png";
-import ImgCarousel5 from "../../Assets/imgs/carousel-5.png";
-import ImgCarousel6 from "../../Assets/imgs/carousel-6.jpg";
 
 const Carousel = () => {
   const settings = {
+    arrows: false,
     autoplay: true,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 2000,
+    dots: true,
     infinite: true,
     speed: 600,
     slidesToShow: 1,
@@ -24,22 +20,40 @@ const Carousel = () => {
     <S.Carousel>
       <Slider {...settings}>
         <div>
-          <img src={ImgCarousel1} alt="Imagem do carrossel" />
+          <img
+            src="/src/Assets/imgs/carousel-1.jpg"
+            alt="Imagem do carrossel"
+          />
         </div>
         <div>
-          <img src={ImgCarousel2} alt="Imagem do carrossel" />
+          <img
+            src="/src/Assets/imgs/carousel-2.png"
+            alt="Imagem do carrossel"
+          />
         </div>
         <div>
-          <img src={ImgCarousel3} alt="Imagem do carrossel" />
+          <img
+            src="/src/Assets/imgs/carousel-3.png"
+            alt="Imagem do carrossel"
+          />
         </div>
         <div>
-          <img src={ImgCarousel4} alt="Imagem do carrossel" />
+          <img
+            src="/src/Assets/imgs/carousel-4.png"
+            alt="Imagem do carrossel"
+          />
         </div>
         <div>
-          <img src={ImgCarousel5} alt="Imagem do carrossel" />
+          <img
+            src="/src/Assets/imgs/carousel-5.png"
+            alt="Imagem do carrossel"
+          />
         </div>
         <div>
-          <img src={ImgCarousel6} alt="Imagem do carrossel" />
+          <img
+            src="/src/Assets/imgs/carousel-6.jpg"
+            alt="Imagem do carrossel"
+          />
         </div>
       </Slider>
     </S.Carousel>
--- src/Components/Carousel/carousel.js
+++ src/Components/Carousel/carousel.js
@@ -2,14 +2,14 @@ import styled from "styled-components";
 import { colors } from "../../Style/globalStyle.js";
 
 export const Carousel = styled.div`
-  width: 80%;
+  width: 40%;
   height: 100%;
 
-  .slick-slide img {
-    width: 70%;
-    height: 70%;
+  .slick-slider img {
+    width: 100%;
+    height: 100%;
     object-fit: cover;
-    border-radius: 1rem;
-    padding: 0.5rem;
+    border-radius: 10px;
+    padding: 5px;
   }
 `;
